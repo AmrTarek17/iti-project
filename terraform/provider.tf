c6c745df59c32be5c1b278e0c25fa290b76fbb35
AmrTarek17 <amro.tarek6@gmail.com>
2023-02-13 14:24:37 +0000

    network fixes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,18 @@ provider "aws"{
   shared_credentials_files = ["/root/.aws/credentials"]
   profile                  = "default"
 }
+
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+  }
+
+  required_version = "~> 1.0"
+}
